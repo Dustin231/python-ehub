08480ba7abb3b73b2517ebd25b0dc2d50ec55132
Andrew <andrew.bollinger@empa.ch>
2018-04-09 17:32:10 +0200

    adding stuff for hslu exercises

--- code/v2.0/main.py
+++ code/v2.0/main.py
@@ -5,13 +5,13 @@
 from run_mod import *
 
 # input data file location
-input_path=r'C:\Users\yam\Documents\GitHub\python-ehub\cases\demos\build v2.0\D5 - multi hub net cen micro.xlsx'
+input_path=r'C:\Users\boa\Documents\Repositories_Github\python-ehub\cases\hslu_exercises\input_data.xlsx'
 
 # results saved to text file location
-result_file = 'C:/Users/yam/Documents/Test cases/Results/results_test.txt'
+result_file = 'C:/Users/boa/Documents/Repositories_Github/python-ehub/results/hslu_exercises/results.txt'
 
 # model parameters saved to text file location
-param_file = 'C:/Users/yam/Documents/Test cases/Results/param_test.txt'
+param_file = 'C:/Users/boa/Documents/Repositories_Github/python-ehub/results/hslu_exercises/params.txt'
 
 # execute model
 run_model(input_path, param_file, result_file) # defined in run_mod.py
