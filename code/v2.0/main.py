a3db09869e9e6816f1640c6420b7713ced01018f
Andrew <andrew.bollinger@empa.ch>
2018-04-12 17:40:33 +0200

    adding eawag experiment input data

--- code/v2.0/main.py
+++ code/v2.0/main.py
@@ -5,13 +5,13 @@
 from run_mod import *
 
 # input data file location
-input_path=r'C:\Users\boa\Documents\Repositories_Github\python-ehub\cases\hslu_exercises\input_data.xlsx'
+input_path=r'C:\Users\boa\Documents\Repositories_Github\python-ehub\cases\eawag_experiment\input_data.xlsx'
 
 # results saved to text file location
-result_file = 'C:/Users/boa/Documents/Repositories_Github/python-ehub/results/hslu_exercises/results.txt'
+result_file = 'C:/Users/boa/Documents/Repositories_Github/python-ehub/results/eawag_experiment/results.txt'
 
 # model parameters saved to text file location
-param_file = 'C:/Users/boa/Documents/Repositories_Github/python-ehub/results/hslu_exercises/params.txt'
+param_file = 'C:/Users/boa/Documents/Repositories_Github/python-ehub/results/eawag_experiment/params.txt'
 
 # execute model
 run_model(input_path, param_file, result_file) # defined in run_mod.py
